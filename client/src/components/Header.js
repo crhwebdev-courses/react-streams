4eb6c253eacdbc3d6ceee4c1eec4effc2c24f4d1
Carl Hain <crhain@gmail.com>
2019-09-25 10:16:14 -0500

    Change name of app to streamy and correct naming issue with class name for right menu item

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,9 +4,10 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <div className="ui secondary pointing menu">
-      <Link to="/" className="Item">
-        Streamer
+      <Link to="/" className="item">
+        Streamy
       </Link>
+
       <div className="right menu">
         <Link to="/" className="item">
           All Streams
