dd90e3461d57b2ee77d54d8a44682be3945ff66e
Carl Hain <crhain@gmail.com>
2019-09-25 11:05:30 -0500

    Update some stuff with GoogleAuth component to make it work correctly

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
+import credentials from '../credentials/';
 
 class GoogleAuth extends Component {
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId: credentials.googleClientId,
+        scope: 'email'
+      });
+    });
+  }
+
   render() {
     return <div className="item">Google Auth</div>;
   }
