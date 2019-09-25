53290259e69402a01f448e61e3c302d889ecb97f
Carl Hain <crhain@gmail.com>
2019-09-25 10:20:41 -0500

    Create stub for GoogleAuth component that will handle authentication and wire it up to header component

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class GoogleAuth extends Component {
+  render() {
+    return <div className="item">Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
