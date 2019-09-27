2880673e09b98896b15c3277517d07ad38420d12
Carl Hain <crhain@gmail.com>
2019-09-26 20:26:53 -0500

    Convert StreamCreate to a class component and hook it up to redux-form

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+class StreamCreate extends Component {
+  render() {
+    return <div>StreamCreate</div>;
+  }
+}
 
-export default StreamCreate;
+export default reduxForm({
+  form: 'streamCreate'
+})(StreamCreate);
