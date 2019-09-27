3e08d034065f5cd3ed73a99db9d0184e49d251ba
Carl Hain <crhain@gmail.com>
2019-09-27 18:40:37 -0500

    client: StreamList prints a list of stream titles

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -3,9 +3,23 @@ import { connect } from 'react-redux';
 import { fetchStreams } from '../../actions';
 
 class StreamList extends Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+
+  renderStreams() {
+    let streams = [];
+
+    for (let key in this.props.streams) {
+      const stream = this.props.streams[key];
+      streams.push(<div key={stream.id}>{stream.title}</div>);
+    }
+
+    return streams;
+  }
+
   render() {
-    console.log(this.props.streams);
-    return <div>StreamList</div>;
+    return <div>{this.renderStreams()}</div>;
   }
 }
 
