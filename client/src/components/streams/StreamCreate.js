de28dce27e8b13f5a6a807beaf146b3dce698a16
Carl Hain <crhain@gmail.com>
2019-09-27 10:54:48 -0500

    StreamCreate shows red text and a red box for any errors in addition to the error message

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -15,8 +15,10 @@ class StreamCreate extends Component {
   }
 
   renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
+
     return (
-      <div className="field">
+      <div className={className}>
         <label>{label}</label>
         <input {...input} />
         {this.renderError(meta)}
