244a2abb7014544295b37d782f43662326be0093
Carl Hain <crhain@gmail.com>
2019-09-27 18:57:38 -0500

    client: add some styling to StreamList

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -9,12 +9,25 @@ class StreamList extends Component {
 
   renderStreams() {
     return this.props.streams.map(stream => {
-      return <div key={stream.id}>{stream.title}</div>;
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
     });
   }
 
   render() {
-    return <div>{this.renderStreams()}</div>;
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">{this.renderStreams()}</div>
+      </div>
+    );
   }
 }
 
