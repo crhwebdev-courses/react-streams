003d47ea1419c0cacef0e524c0bd6ef44db2f818
Carl Hain <crhain@gmail.com>
2019-09-26 21:05:57 -0500

    StreamCreate now has a submit button and will log form values to console when the form is submitted

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -11,15 +11,23 @@ class StreamCreate extends Component {
     );
   }
 
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
   render() {
     return (
-      <form className="ui form">
+      <form
+        className="ui form"
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+      >
         <Field name="title" label="Enter Title" component={this.renderInput} />
         <Field
           name="description"
           label="Enter Description"
           component={this.renderInput}
         />
+        <button className="ui button primary">Submit</button>
       </form>
     );
   }
