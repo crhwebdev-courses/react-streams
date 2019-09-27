e6aa09f23289610b3646680d51e35ffff7b761bd
Carl Hain <crhain@gmail.com>
2019-09-26 20:52:33 -0500

    StreamCreate shows a basic form with title and description fields

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,8 +2,17 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends Component {
+  renderInput({ input }) {
+    return <input {...input} />;
+  }
+
   render() {
-    return <div>StreamCreate</div>;
+    return (
+      <form>
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
+      </form>
+    );
   }
 }
 
