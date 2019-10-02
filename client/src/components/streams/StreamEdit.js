3f9a70008efc5baecddfaaa1e308af18421468df
Carl Hain <crhain@gmail.com>
2019-10-02 10:07:40 -0500

    StreamEdit now retrieves its associated stream when loading

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { fetchStream } from '../../actions/';
 
 class StreamEdit extends Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
   render() {
     if (this.props.stream) {
       return <div>{this.props.stream.title}</div>;
@@ -15,4 +20,7 @@ const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps)(StreamEdit);
+export default connect(
+  mapStateToProps,
+  { fetchStream }
+)(StreamEdit);
