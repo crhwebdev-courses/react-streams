3c4010c7b5b8a2bf49a3673004547cc390e4b21c
Carl Hain <crhain@gmail.com>
2019-10-04 15:32:23 -0500

    StreamEdit form shows a form with initial values from stream that is being edited

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchStream } from '../../actions/';
+import { fetchStream, editStream } from '../../actions/';
+import StreamForm from './StreamForm';
 
 class StreamEdit extends Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
+  onSubmit = formValues => {
+    console.log(formValues);
+  };
 
   render() {
-    if (this.props.stream) {
-      return <div>{this.props.stream.title}</div>;
-    }
-
-    return <div>Loading stream...</div>;
+    console.log(this.props.stream);
+    return (
+      <div>
+        <h3>Edit Stream</h3>
+        <StreamForm
+          onSubmit={this.onSubmit}
+          initialValues={this.props.stream}
+        />
+      </div>
+    );
   }
 }
 
@@ -22,5 +31,5 @@ const mapStateToProps = (state, ownProps) => {
 
 export default connect(
   mapStateToProps,
-  { fetchStream }
+  { fetchStream, editStream }
 )(StreamEdit);
