5ef3b53ef45fa9b746fae0435286221cfefe71c8
Carl Hain <crhain@gmail.com>
2019-10-05 08:36:30 -0500

    Fix issue with Modal not returning anything! Now it does

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Modal = props => {
-  ReactDOM.createPortal(
+  return ReactDOM.createPortal(
     <div className="ui dimmer modals visible active">
       <div className="ui standard modal visible active">Hi!</div>
     </div>,
