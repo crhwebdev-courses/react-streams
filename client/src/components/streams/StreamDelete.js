2c39422b30252b7dab5404a7461cc9a20999eab6
Carl Hain <crhain@gmail.com>
2019-10-11 09:23:38 -0500

    Refactor StreamDelete to be a class component so we can add some lifestyle methods

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,29 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Modal from '../Modal';
 import history from '../../history';
 
-const StreamDelete = () => {
+class StreamDelete extends Component {
   //use of react fragments: renders an invisible element so you can
   // render multiple elements in an invisible element without needing to wrap them in
   // a container element
-  const actions = (
-    <React.Fragment>
-      <button className="ui button negative">Delete</button>
-      <button className="ui button">Cancel</button>
-    </React.Fragment>
-  );
 
-  return (
-    <div>
-      StreamDelete
-      <Modal
-        title="Delete Stream"
-        content="Do you want to delete this stream?"
-        actions={actions}
-        onDismiss={() => history.push('/')}
-      />
-    </div>
-  );
-};
+  renderActionButtons() {
+    return (
+      <React.Fragment>
+        <button className="ui button negative">Delete</button>
+        <button className="ui button">Cancel</button>
+      </React.Fragment>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        StreamDelete
+        <Modal
+          title="Delete Stream"
+          content="Do you want to delete this stream?"
+          actions={this.renderActionButtons()}
+          onDismiss={() => history.push('/')}
+        />
+      </div>
+    );
+  }
+}
 
 export default StreamDelete;
