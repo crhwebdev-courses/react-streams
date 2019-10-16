724d45c8f19449bd71fba8efb0dd09b2c347c8d9
Carl Hain <crhain@gmail.com>
2019-10-15 20:13:09 -0500

    Fix issue with multiple routes showing at same time by using Switch component in App

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
@@ -11,16 +11,20 @@ import history from '../history';
 const App = () => {
   // using Router instead of BrowserRouter because we are creating
   // our own history object in history.js
+  // using Switch component to only show one route at a time
+  // because /streams/new and /streams/:id will both show at same time otherwise
   return (
     <div className="ui container">
       <Router history={history}>
         <div>
           <Header />
-          <Route path="/" exact component={StreamList} />
-          <Route path="/streams/new" exact component={StreamCreate} />
-          <Route path="/streams/edit/:id" exact component={StreamEdit} />
-          <Route path="/streams/delete/:id" exact component={StreamDelete} />
-          <Route path="/streams/:id" exact component={StreamShow} />
+          <Switch>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/edit/:id" exact component={StreamEdit} />
+            <Route path="/streams/delete/:id" exact component={StreamDelete} />
+            <Route path="/streams/:id" exact component={StreamShow} />
+          </Switch>
         </div>
       </Router>
     </div>
